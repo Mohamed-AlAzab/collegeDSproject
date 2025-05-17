93d12e7549f3a9ccd8487908f60263ade80103cf
Abdallah Ahmed <148303169+Iabdallllah@users.noreply.github.com>
2025-05-18 01:17:33 +0300

    Update LinearHashing.java

--- src/LinearHashing.java
+++ src/LinearHashing.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Linear {
-    private int size;
-    private String[] hashtable;
-    final int m = (int) (Math.pow(10, 9) + 9);
+    private int size; 
+    private String[] hashtable; 
+    final int m = (int) (Math.pow(10, 9) + 9); // Large prime number for less collision
     private HashMap<String, ArrayList<Integer>> collisionPlaces = new HashMap<>();
+    // Stores number of collisions for each word
     private HashMap<String, Integer> collisionTimes = new HashMap<>();
 
     public Linear(int capacity) {
@@ -16,6 +17,7 @@ public class Linear {
         hashtable = new String[size];
     }
 
+    // Constructor that sets size based on input word list
     public Linear(ArrayList<String> word) {
         size = word.size();
         hashtable = new String[size];
@@ -25,34 +27,37 @@ public class Linear {
         return (int) ((c * Math.pow(31, (n - 1 - i))) % m);
     }
 
+    // Insert a list of strings into the hash table 
     public void insert(ArrayList<String> strings) {
         for (String word : strings) {
             int tech = 0;
+            // Calculate hash value for the word
             for (int j = 0; j < word.length(); j++) {
                 char c = word.charAt(j);
                 tech += Technique(c, j, word.length());
             }
             int i = 0;
             boolean inserted = false;
-            ArrayList<Integer> places = new ArrayList<>();
+            ArrayList<Integer> places = new ArrayList<>(); // To record collision indices
             int collisions = 0;
             while (i < size) {
                 int index = (tech + i) % size;
+                // Prevent infinite loop if table is full
                 if (i > 0 && index == tech) {
                     break;
                 }
                 if (hashtable[index] == null) {
-                    hashtable[index] = word;
+                    hashtable[index] = word; // Insert word if slot is empty
                     inserted = true;
                     break;
                 } else {
-                    collisions++;
-                    places.add(index);
+                    collisions++; 
+                    places.add(index); 
                 }
                 i++;
             }
-            collisionPlaces.put(word, places);
-            collisionTimes.put(word, collisions);
+            collisionPlaces.put(word, places); 
+            collisionTimes.put(word, collisions); 
 
             if (!inserted) {
                 System.out.println("The table is full❌ or infinite loop occured at word: " + word);
@@ -84,14 +89,14 @@ public class Linear {
             String originalHash = "";
             int collisionsAtIndex = 0;
             if (word != null) {
-                // cal original hash
+                // Calculate original hash for display
                 int tech = 0;
                 for (int j = 0; j < word.length(); j++) {
                     char c = word.charAt(j);
                     tech += Technique(c, j, word.length());
                 }
                 originalHash = String.valueOf(tech % size);
-                // num of coli in this place
+                // Count how many collisions in this index 
                 collisionsAtIndex = 0;
                 for (ArrayList<Integer> places : collisionPlaces.values()) {
                     for (int idx : places) {
@@ -103,7 +108,7 @@ public class Linear {
         }
         System.out.println("+-------------+----------------+---------------+---------------------+");
     }
-
+    
     public int getCollisionCount() {
         int total = 0;
         for (int count : collisionTimes.values()) {
